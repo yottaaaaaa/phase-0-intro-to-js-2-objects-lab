5da524194a0e0f14e7d82d0565cd48f39ecde967
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-04-07 23:28:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    if (!employee) {
+        throw new Error('Employee object is not defined');
+    }
+    const updatedEmployee = Object.assign({}, employee);
+    updatedEmployee[key] = value;
+
+    return updatedEmployee;
+}
+
+
+const employee = {
+    name: 'John',
+    age: 30,
+    department: 'HR'
+};
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'salary', 50000);
+
+console.log(updatedEmployee);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    if (!employee) {
+        throw new Error('Employee object is not defined');
+    }
+    employee[key] = value;
+
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    if (!employee) {
+        throw new Error('Employee object is not defined');
+    }
+    const updatedEmployee = Object.assign({}, employee);
+
+    if (updatedEmployee.hasOwnProperty(key)) {
+        delete updatedEmployee[key];
+    }
+
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    if (!employee) {
+        throw new Error('Employee object is not defined');
+    }
+
+    if (employee.hasOwnProperty(key)) {
+        
+        delete employee[key];
+    }
+    return employee;
+}
+
+
+
+
+
+
+
